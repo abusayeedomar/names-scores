c4ac1674ab6e214ae8b4896b772fad6f9230f675
Abu Sayeed Omar <abusayeedomar@users.noreply.github.com>
2015-11-21 16:56:06 +0000

    Create name-scores.py

---
+++ name-scores.py
@@ -0,0 +1,32 @@
+fname = "names.txt"
+
+#Random name order 
+#with open(fname) as f:
+    #content = f.readlines()
+    #random.shuffle(content)
+    #for s in content:
+    #    print s
+
+with open(fname) as f:
+    content = f.readlines()
+    #for s in content:
+    #    print s
+    count = 0
+    for x in sorted(content):
+        count += 1
+        x = x.lower().rstrip('\n')
+        #print x
+        namelist = list(x)
+            
+        charnum = []
+        for character in list(x):
+            number = ord(character)-96
+            #print number
+            charnum.insert(0, number)
+        #print charnum
+        charnum = list(reversed(charnum))
+        charcnumsum = sum(charnum)
+        result = count * charcnumsum
+        print str(count) + '. ' + x.title() + ' (' + str(count) + ' x ' + str(charcnumsum) + ') = ' + str(result)
+        result += result
+    print '#---------------------------------------------------------\nTotal of all the name scores in file = ' + str(result) + '\n---------------------------------------------------------#'
